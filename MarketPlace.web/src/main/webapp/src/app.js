166a600fe6fb3db7d127f8d7e0d1a1cd20c2f996
Jhonatan A <jhonatan.alarcon.ariza@gmail.com>
2015-08-13 10:27:34 -0500

    Configuracion libreria auth para aplicacion principal

--- MarketPlace.web/src/main/webapp/src/app.js
+++ MarketPlace.web/src/main/webapp/src/app.js
@@ -14,21 +14,36 @@
 
     mainApp.config(['$routeProvider', 'CrudTemplateURL', 'CrudCtrlAlias', function ($routeProvider, tplUrl, alias) {
             $routeProvider
-                .when('/book', {
-                    templateUrl: tplUrl,
-                    controller: 'bookCtrl',
-                    controllerAs: alias
-                })
-                .when('/client', {
-                    templateUrl: tplUrl,
-                    controller: 'clientCtrl',
-                    controllerAs: alias
-                })
-                .when('/provider', {
-                    templateUrl: tplUrl,
-                    controller: 'providerCtrl',
-                    controllerAs: alias
-                })
-                .otherwise('/');
+                    .when('/book', {
+                        templateUrl: tplUrl,
+                        controller: 'bookCtrl',
+                        controllerAs: alias
+                    })
+                    .when('/client', {
+                        templateUrl: tplUrl,
+                        controller: 'clientCtrl',
+                        controllerAs: alias
+                    })
+                    .when('/provider', {
+                        templateUrl: tplUrl,
+                        controller: 'providerCtrl',
+                        controllerAs: alias
+                    })
+                    .otherwise('/');
         }]);
+
+    mainApp.config(['authServiceProvider', function (auth) {
+            auth.setValues({
+                apiUrl: 'users',
+                successPath: '/book',
+                loginPath: '/login',
+                registerPath: '/register',
+                logoutRedirect: '/login',
+                loginURL: 'login',
+                registerURL: 'register',
+                logoutURL: 'logout',
+                nameCookie: 'userCookie'
+            });
+        }]);
+
 })(window.angular);
