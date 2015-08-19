69a84751df08473481e1cca3a702a17a7246e21b
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-08-19 11:27:03 -0500

    Corrección redirect

--- MarketPlace.web/src/main/webapp/src/modules/product/product.ctrl.js
+++ MarketPlace.web/src/main/webapp/src/modules/product/product.ctrl.js
@@ -1,38 +1,43 @@
 (function (ng) {
     var mod = ng.module('productModule');
 
-    mod.controller('productCtrl', ['CrudCreator', '$scope', 'productService', 'productModel', 'cartItemService', '$location', 'authService', function (CrudCreator, $scope, svc, model, cartItemSvc, $location, authService) {
-            CrudCreator.extendController(this, svc, $scope, model, 'product', 'Products');
+    mod.controller('productCtrl', ['CrudCreator', '$scope', 'productService', 'productModel', 'cartItemService', '$location', 'authService', function (CrudCreator, $scope, svc, model, cartItemSvc, $location, authSvc) {
+            if (!authSvc.getCurrentUser()) {
+                $location.path('/login');
+            } else {
+                CrudCreator.extendController(this, svc, $scope, model, 'product', 'Products');
 
-            this.searchByName = function (bookName) {
-                var search;
-                if (bookName) {
-                    search = '?q=' + bookName;
-                }
-                $location.url('/catalog' + search);
-            };
-
-            this.recordActions = [{
-                    name: 'addToCart',
-                    displayName: 'Add to Cart',
-                    icon: 'shopping-cart',
-                    class: 'primary',
-                    fn: function (record) {
-                        if (authService.getCurrentUser()) {
-                            return cartItemSvc.saveCurrentRecord({
-                                product: record,
-                                name: record.book.name,
-                                quantity: 1});
-                        }else{
-                            $location.path('/login');// o colocar servicio de LocalStorage
-                        }
-                    },
-                    show: function () {
-                        return true;
+                this.searchByName = function (bookName) {
+                    var search;
+                    if (bookName) {
+                        search = '?q=' + bookName;
                     }
-                }];
+                    $location.url('/catalog' + search);
+                };
 
-            this.loadRefOptions();
-            this.fetchRecords();
+                this.recordActions = [{
+                        name: 'addToCart',
+                        displayName: 'Add to Cart',
+                        icon: 'shopping-cart',
+                        class: 'primary',
+                        fn: function (record) {
+                            if (authSvc.getCurrentUser()) {
+                                return cartItemSvc.saveCurrentRecord({
+                                    product: record,
+                                    name: record.book.name,
+                                    quantity: 1});
+                            } else {
+                                $location.path('/login');// o colocar servicio de LocalStorage
+                            }
+                        },
+                        show: function () {
+                            return true;
+                        }
+                    }];
+
+                this.loadRefOptions();
+                this.fetchRecords();
+            }
+
         }]);
 })(window.angular);
