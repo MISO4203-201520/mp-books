1c9bbf08c6a1ddeec0c0dfe97b0897e5fc846b53
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-08-20 17:35:04 -0500

    Gestión de productos - Se habilita al proveedor gestionar sus productos.

--- MarketPlace.web/src/main/webapp/src/modules/product/product.mod.js
+++ MarketPlace.web/src/main/webapp/src/modules/product/product.mod.js
@@ -5,16 +5,16 @@
 
     mod.constant('productModel', {
         fields: [{
-                name: 'price',
-                displayName: 'Price',
-                type: 'Currency',
-                required: true
-            }, {
                 name: 'book',
                 displayName: 'Book',
                 type: 'Reference',
                 service: 'bookService',
                 options: [],
+                required: true
+            }, {
+                name: 'price',
+                displayName: 'Price',
+                type: 'Currency',
                 required: true
             }]});
 })(window.angular);
